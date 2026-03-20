2b42fb48788b79ca1c1f8444d3fd6c51b7992b34
rmKitKata <katarina.ab1907@gmail.com>
2026-03-20 16:59:35 -0600

    Remaps inecesarios eliminados, n y N no tienen mapeos
    No se ni para qué eran me faltó odio

--- lua/kata/remap.lua
+++ lua/kata/remap.lua
@@ -10,8 +10,6 @@ vim.api.nvim_set_keymap("n", "<leader>tf", "<Plug>PlenaryTestFile", { noremap = 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "=ap", "ma=ap'a")
 
 -- greatest remap ever
